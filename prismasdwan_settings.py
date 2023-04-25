3d28594df51b45e28dede0ec51711bba7ab4f6a2
Tanushree <34695939+ktanushree@users.noreply.github.com>
2023-04-25 11:24:45 -0400

    Add files via upload

---
+++ prismasdwan_settings.py
@@ -0,0 +1,11 @@
+#
+# Prisma SD-WAN Auth Token
+#
+CLOUDGENIX_AUTH_TOKEN="paste auth token"
+#
+# Service Account
+#
+PRISMASDWAN_CLIENT_ID="paste client ID"
+PRISMASDWAN_CLIENT_SECRET="paste client secret"
+PRISMASDWAN_TSG_ID="paste Tenant TSG ID"
+
